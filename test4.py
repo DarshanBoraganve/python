e44af2a2eeeb1b196137c73722f15eb75972502e
DarshanBoraganve <darshanboraganve2003@gmail.com>
2024-09-03 22:03:58 +0530

    Init function

---
+++ test4.py
@@ -0,0 +1,12 @@
+
+
+class programmer:
+    def __init__(self,name,product):
+        self.name=name
+        self.product=product
+    def info(self):
+        print(f" the name of the programmer is {self.name} and the product is{self.product}")
+harry=programmer("darshan"," skype")
+alka=programmer("rushikesh", " reva")
+harry.info()
+alka.info()
